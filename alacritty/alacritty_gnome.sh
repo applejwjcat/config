d03721631b9fc61360f4131614332071d740cbd7
Tristan <15997232823@163.com>
2020-04-17 11:03:05 +0800

    some cobfig

---
+++ alacritty/alacritty_gnome.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+#########################################################################
+# File Name: alacritty_gnome.sh
+# Author: Tristan
+# mail: 15997232823@163.com
+# Last Modified: 2020年04月08日 星期三 20时35分50秒
+# Created Time: 2020年04月08日 星期三 19时50分00秒
+#########################################################################
+
+sed -i '125s/10/14/' /home/appledog/.config/alacritty/alacritty.yml
